b56ab9e47a4c297d966b3b3b95895e6ac98ffdb7
Merve Bayram Durna <mervebdurna@gmail.com>
2021-12-03 01:45:01 +0100

    Update 1.sql_select.sql

--- 1.sql_select.sql
+++ 1.sql_select.sql
@@ -1,38 +1,47 @@
 --case1 :  get all columns
-select * from table_name
+select *
+from table_name
 
 --case2 :  gel columns with alias
-select  name as Isim,
-        surname as Soyisim
+select  Name as NameColumn,
+        Surname as SurnameColumn
 from table_name
 
 --case 3: get columns with column positions (available in Snowflake)
-select  $1,$2
+select  $1,
+        $2
 from table_name
 
 --case 4: make columns case sensitive by using double quote( "" )
-select "Name","Surname"
+select "Name",
+       "Surname"
 from table_name
 
 -- case5: get distinct value from the columns
-select distinct(name)
+select distinct(Name)
 from table_name
 
 -- case6: get number of distinct value 
-select count(distinct name)
+select count(distinct Name)
 from table_name
 
 -- case7: recoding 'null' values with COALESCE 
 -- SOURCE : https://towardsdatascience.com/6-sql-tricks-every-data-scientist-should-know-f84be499aea5
-select null_column,
-       COALESCE(null_column, 'Missing') as encoded_null_colum
+select NullColumn,
+       coalesce(NullColumn, 'Missing') as EncodedNullColumn
 from table_name
 
 -- case8: recoding empty and 'NA' values
--- NOTE :COALESCE don't work with 'NA' and empty instead CASE WHEN statement can be used
-select na_column,
-       CASE WHEN na_column = 'NA' THEN 'NA_Missing' END as encoded_na_column,
-       CASE WHEN empty_column = '' THEN 'Empty_Missing' END as encoded_empty_column
+-- NOTE :COALESCE don't work with 'NA' and empty instead case when statement can be used
+select NaColumn,
+       case when NaColumn = 'NA' 
+       	    then 'NA_Missing' 
+	    else NaColumn
+	    end as EncodedNaColumn,
+       case when EmptyColumn = '' 
+            then 'Empty_Missing' 
+	    else EmptyColumn
+	    end as EncodedEmptyColumn
 from table_name
 
 --NOTE : 
@@ -44,7 +53,8 @@ from table_name
 --(4) HAVING
 --(6) ORDER BY
 
--- case9: SCALAR FUNCTIONS USAGE (Scalar functions, like in other programming languages, return a scalar value based on the given arguments.)
+-- case9: scalar functions usage 
+--(Scalar functions, like in other programming languages, return a scalar value(!! one) based on the given arguments.)
 -- DATEADD (datepart, number, date) - adds (or subtracts) the number of date 
 -- DATEDIFF (datepart, startdate, enddate) - difference between two dates (end - start) 
 -- DATEPART (datepart, date) - extracts the specified datepart parameter,
@@ -53,22 +63,24 @@ from table_name
 select Name,
        Surname,
        BirthDate, 
-       YEAR(BirthDate) as BirthYear,
-       DATEDIFF(yy,BirthDate, getdate()) as Age
+       year(BirthDate) as BirthYear,
+       datediff(yy,BirthDate, getdate()) as Age
  from Employee
 
 
 
-SELECT DATEADD (dd, -DAY (GETDATE () - 1), GETDATE ()) as FirstDayCurrMonth,
-	DATEADD (dd, -DAY (GETDATE ()), GETDATE ()) as LastDayPrevMonth
+select dateadd(dd, -DAY (GETDATE () - 1), GETDATE ()) as FirstDayCurrMonth,
+	dateadd(dd, -DAY (GETDATE ()), GETDATE ()) as LastDayPrevMonth
 
 
-SELECT DATENAME (dw, GETDATE ()) as Weekday,
-	DATENAME (mm, GETDATE ()) as Month
+select datename(dw, detdate ()) as Weekday,
+	datename(mm, getdate ()) as Month
+
+
 
 --case 10 :Artihmetic expresion usage
--- calculate without a table;) 
-Select 2 + 2 as the result
+-- calculations without a table
+select 2 + 2 as Result
 
 -- Calculations on the values from the columns 
 -- ROUND function rounds the result to n decimal places
@@ -76,14 +88,17 @@ Select 2 + 2 as the result
 -- CEILING (fractional_number) works similarly - it always rounds up to the full value
 
 select UnitPrice, Quantity, Discount, 
-	Round ((UnitPrice * Quantity * (1-Discount)) * 1.23, 2) as TotalBrutto
-from dbo. [Order Details]
+	round((UnitPrice * Quantity * (1-Discount)) * 1.23, 2) as TotalBrutto
+from dbo.[Order Details]
 
-SELECT FLOOR (123.78), FLOOR (-123.78), CEILING (123.78), CEILING (-123.78)
+select floor (123.78) as FloorPositiveNumber, 
+       floor (-123.78) as FloorNegati veNumber, 
+       ceiling (123.78) as CeilingPositiveNumber, 
+       ceiling (-123.78) as CeilingNegativeNumber
 --source : https://www.sqlpedia.pl/funkcje-matematyczne-w-sql/
 
 
 --case 11 :Text expresion usage
-select LastName + '' + FirstName as FirstName, 
-       LEFT (FirstName, 1) + '. '+ LastName as FirstName2
+select LastName + ' ' + FirstName as FirstName, 
+       left (FirstName, 1) + '. '+ LastName as FirstName2
 from dbo.Employees
