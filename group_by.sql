e5fc8944a59429a81006b212da5351ffbca48567
Merve Bayram Durna <mervebdurna@gmail.com>
2021-12-07 20:29:33 +0100

    Update and rename group_by to group_by.sql

---
+++ group_by.sql
@@ -0,0 +1,13 @@
+--ORDER BY 
+
+Note : The LIMIT will reduce the number of rows to return, and the optional OFFSET will specify where to begin counting the number rows from.
+SOURCE : https://sqlbolt.com/lesson/filtering_sorting_query_results
+
+SELECT column, another_column, …
+FROM mytable
+WHERE condition(s)
+ORDER BY column ASC/DESC
+LIMIT num_limit OFFSET num_offset;
+
+--GROUP BY 
+
